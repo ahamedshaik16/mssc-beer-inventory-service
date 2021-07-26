afcf763aaa8ddcb8072e6f740cae452f6fc22433
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-26 11:19:23 +0530

    Updated port

--- src/main/java/guru/sfg/beer/inventory/service/web/model/BeerInventoryDto.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/model/BeerInventoryDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.awt.*;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -20,5 +21,6 @@ public class BeerInventoryDto {
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
     private UUID beerId;
+    private String upc;
     private Integer quantityOnHand;
 }
